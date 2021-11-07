049b0d890800d78c21846fea134715b70a16aad0
Murat Gözel <murat@gozel.com.tr>
2021-11-07 09:33:06 +0300

    debug storage.reserveId

--- src/Storage.js
+++ src/Storage.js
@@ -12,7 +12,7 @@ export default class Storage {
   async reserveId(col='id') {
     const tableToken = sql.identifier([this.name])
     const fieldToken = sql.identifier([col])
-    const query = sql`select nextval(pg_get_serial_sequence('${this.name}', '${col}')) as new_id`
+    const query = sql`select nextval(pg_get_serial_sequence(${tableToken}, ${fieldToken})) as new_id`
     const result = await this.pgpool.query(query)
     console.log('result:', result)
     return result.rows[0].new_id
